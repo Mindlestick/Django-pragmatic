ea18c5e67f8af3836af500bf92b5666528769a1b
Mindlestick <iorw0224@gmail.com>
2023-02-10 01:53:20 +0900

    django course 11 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -124,6 +124,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static", #정적파일 모아서 관리
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
