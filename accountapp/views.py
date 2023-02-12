4315bd7ceef1ebd9b5c4a3deadce32115db199e2
Mindlestick <iorw0224@gmail.com>
2023-02-13 00:29:45 +0900

    DB에 데이터 저장

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,10 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
 
 
 def hello_world(request):
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!!!'}) #context : 데이터 꾸러미
+        temp = request.POST.get('hello_world_input') #request의 POST중에서 가져와라
+
+        new_hello_world = HelloWorld() #이 빵틀에서 나온 새로운 객체 저장
+        new_hello_world.text = temp
+        new_hello_world.save() #DB에 저장
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world}) #context : 데이터 꾸러미
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
