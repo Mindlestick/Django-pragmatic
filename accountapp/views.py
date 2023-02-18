ebfc6faf594d84352aba163245b759a0557acbfb
Mindlestick <iorw0224@gmail.com>
2023-02-18 22:42:23 +0900

    is_authenticated

--- accountapp/views.py
+++ accountapp/views.py
@@ -12,18 +12,22 @@ from accountapp.models import HelloWorld
 
 
 def hello_world(request):
-    if request.method == "POST":
-        temp = request.POST.get('hello_world_input') #request의 POST중에서 가져와라
+    # 로그인 되어있으면
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            temp = request.POST.get('hello_world_input')  # request의 POST중에서 가져와라
 
-        new_hello_world = HelloWorld() #이 빵틀에서 나온 새로운 객체 저장
-        new_hello_world.text = temp
-        new_hello_world.save() #DB에 저장
+            new_hello_world = HelloWorld()  # 이 빵틀에서 나온 새로운 객체 저장
+            new_hello_world.text = temp
+            new_hello_world.save()  # DB에 저장
 
-        return HttpResponseRedirect(reverse('accountapp:hello_world')) #이 주소로 돌아갈 수 있게 재접속 (urls에서) reverse로 감싸줘야됨
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))  # 이 주소로 돌아갈 수 있게 재접속 (urls에서) reverse로 감싸줘야됨
+        else:
+            hello_world_list = HelloWorld.objects.all()  # DB에 있는 모든 데이터 불러오기
+            return render(request, 'accountapp/hello_world.html',
+                          context={'hello_world_list': hello_world_list})  # context : 데이터 꾸러미
     else:
-        hello_world_list = HelloWorld.objects.all()  # DB에 있는 모든 데이터 불러오기
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list}) #context : 데이터 꾸러미
-
+       return HttpResponseRedirect(reverse('accountapp:login'))
 
 class AccountCreateView(CreateView):
     # 1. 무슨 모델을 사용할 것인지?
