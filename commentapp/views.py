688945d0c23b88dde211f6759b9dce824acc4b43
Mindlestick <iorw0224@gmail.com>
2023-03-10 04:30:27 +0900

    댓글 시각화, 삭제

--- commentapp/views.py
+++ commentapp/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DeleteView
 
 from articleapp.models import Article
+from commentapp.decorators import comment_ownership_required
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
@@ -25,7 +27,8 @@ class CommentCreateView(CreateView):
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
 
-
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
 class CommentDeleteView(DeleteView):
     model = Comment
     context_object_name = 'target_comment'
