4477a9064cb6bb1c84274335a5cf9342d5d30fb9
Mindlestick <iorw0224@gmail.com>
2023-03-10 03:59:29 +0900

    오타수정

--- commentapp/views.py
+++ commentapp/views.py
@@ -17,7 +17,7 @@ class CommentCreateView(CreateView):
     # form_valid를 원하는 데로 변경
     def form_valid(self, form):
         temp_comment = form.save(commit=False)
-        temp_comment.article = Article.object.get(pk=self.request.POST('article_pk'))
+        temp_comment.article = Article.objects.get(pk=self.request.POST('article_pk'))
         temp_comment.writer = self.request.user
         temp_comment.save()
         return super().form_valid(form)
