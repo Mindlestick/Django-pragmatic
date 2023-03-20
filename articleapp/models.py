3e322f3a76d6f04470f9ea035b185ce2145d2dce
Mindlestick <iorw0224@gmail.com>
2023-03-20 22:19:10 +0900

    Project와 Article 연결

--- articleapp/models.py
+++ articleapp/models.py
@@ -2,10 +2,15 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
 
 class Article(models.Model):
+    #누가 썼는지
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article',null=True)
 
+    #어느 게시판인지
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article',null=True)
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=False)
     content = models.TextField(null=True)
