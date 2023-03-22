6f11340259de25a169fe1e02576cf41547b3c2cd
Mindlestick <iorw0224@gmail.com>
2023-03-23 00:31:07 +0900

    게시글 작성시 project를 선택하지 않아도 업로드 가능하게 하는 옵션

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -2,11 +2,14 @@ from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationFrom(ModelForm):
     content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable text-start',
                                                            'style': 'height: auto;'}))
+    #게시글 작성시 project를 선택하지 않아도 되게 하는 옵션
+    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
 
     class Meta:
         model = Article
